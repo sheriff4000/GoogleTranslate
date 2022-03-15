b6eba1f8021d23f63a4c6d1a0863b26fdfb2cb7a
sheriff4000 <sheriff4000@gmail.com>
2022-03-15 16:26:30 +0000

    .

--- include/ast/context.hpp
+++ include/ast/context.hpp
@@ -1,39 +1,15 @@
 #ifndef context_hpp
 #define context_hpp
+
 #include<unordered_map>
 #include <vector>
 
 
 #include<unordered_map>
 
-#include "ast/ast_node.hpp"
-#include "ast/registers.hpp"
-#include "ast/functions/ast_function_def.hpp"
+
+
 struct function;
-
-class registers;
-
-class context
-{
-private:
-    //variables storing register configuration, function names mapped to pointers, idk other context stuff
-    registers reg;
-    std::unordered_map <std::string, function> function_size;//not 100% sure about what maps to what, but it's ok for now
-
-public:
-    //functions to check context should be here
-    // int get_reg()
-    // {
-    //     return reg.get_reg();
-    // }
-    // nodePtr get_function(std::string name)
-    // {
-    //     return function_defs[name];
-    // }
-
-
-};
-
 
 struct function{
     int size;
@@ -57,3 +33,29 @@ public:
         return -1;
     }
 };
+
+class context
+{
+private:
+    //variables storing register configuration, function names mapped to pointers, idk other context stuff
+    registers reg;
+    std::unordered_map <std::string, function> function_size;//not 100% sure about what maps to what, but it's ok for now
+
+public:
+    //functions to check context should be here
+    // int get_reg()
+    // {
+    //     return reg.get_reg();
+    // }
+    // nodePtr get_function(std::string name)
+    // {
+    //     return function_defs[name];
+    // }
+
+
+};
+
+
+
+
+#endif
