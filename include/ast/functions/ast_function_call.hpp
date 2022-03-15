c2437a43055123ae48238b1da856a12fe158af73
sheriff4000 <sheriff4000@gmail.com>
2022-03-15 13:24:23 +0000

    .

--- include/ast/functions/ast_function_call.hpp
+++ include/ast/functions/ast_function_call.hpp
@@ -6,7 +6,7 @@
 class function_call : public node {
 private: 
     nodePtr def; //points to the definition object, perhaps we could use the prexisting branches variable inherited from node class.
-    std::vector<nodePtr> args
+    std::vector<nodePtr> args;
 public :
     function_call(nodePtr _def, std::vector<nodePtr> _args)
     {
@@ -17,8 +17,9 @@ public :
     void gen_mips()
     {
         //we access the function definition and pass it the args we have for this specfic call?
-        def->gen_mips(args)
-        //so all the making a new stakc frame, frame pointers, return pointers etc. is written in the function_def class function gen_mips
+        
+        def->gen_mips(args);
+        //so all the making a new stack frame, frame pointers, return pointers etc. is written in the function_def class function gen_mips
     }
 
 };
