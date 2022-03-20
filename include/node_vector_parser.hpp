d27df9822afd1c9b19c69d1298b41e8697dcfe99
sheriff4000 <sheriff4000@gmail.com>
2022-03-20 15:58:03 +0000

    .

--- include/node_vector_parser.hpp
+++ include/node_vector_parser.hpp
@@ -8,7 +8,7 @@ typedef std::vector<node_ptr> node_vector;
 typedef node_vector* node_vector_ptr;
 
 
-node_vector_ptr new_vector(node_ptr node)
+inline node_vector_ptr new_vector(node_ptr node)
 {
     //we want to make a new vector (dynamically) and return its pointer after adding the passed node
     node_vector_ptr out = new node_vector;
