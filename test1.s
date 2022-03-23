3364ed88e47999c2e036cd155f85f58b434e2098
sheriff4000 <sheriff4000@gmail.com>
2022-03-23 16:39:24 +0000

    .

--- test1.s
+++ test1.s
@@ -7,3 +7,12 @@ lw      $fp,4($sp)
 addiu   $sp,$sp,8
 j       $31
 nop
+hello:
+addiu   $sp,$sp,-8
+sw      $fp,4($sp)
+move    $fp,$sp
+move    $sp,$fp
+lw      $fp,4($sp)
+addiu   $sp,$sp,8
+j       $31
+nop
