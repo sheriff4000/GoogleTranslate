d736ea28a045424baecf63280b1b4a6b1fa760db
sheriff4000 <sheriff4000@gmail.com>
2022-03-27 20:42:44 +0100

    .

--- include/ast/ast_declarator.hpp
+++ include/ast/ast_declarator.hpp
@@ -3,10 +3,11 @@
 class declarator : public node
 {
     private:
-
+        std::vector<node_ptr> args;
+        
     public:
-        declarator(node_ptr _type, node_vector_ptr args)
+        declarator(node_ptr _type, node_vector_ptr _args)
         {
-
+            args = *_args;
         }
 };
